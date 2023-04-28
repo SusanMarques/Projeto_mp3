f33dda79aecfb6765355f371d2d4545ea88656f1
SUSAN KARIN  SOARES MARQUES <susan.marques@saude.govrn>
2023-04-28 13:11:21 -0300

    Audios teste

--- pages/musics.php
+++ pages/musics.php
@@ -1,6 +1,12 @@
 <h1> Músicas do Álbum <?=$_GET['album']?></h1>
 <hr>
-<audio src="albums/empires"></audio>
+<div class="col-12">
+    <audio src="albums/empires/musics/guitar.mp3" controls></audio>
+</div>
+<div>
+    <audio src="albums/grace/musics/guitar2.mp3" controls></audio>
+</div>
+<div>
+    <audio src="albums/wonder/musics/guitar3.mp3" controls></audio>
+</div>
 
-
-teste
